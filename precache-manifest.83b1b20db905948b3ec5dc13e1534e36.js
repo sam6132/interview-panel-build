6a61ee225b80f370db894932648e2b7470e32aa3
sam joshuva <sam613263@gmail.com>
2019-06-13 12:19:21 +0530

    as

---
+++ precache-manifest.83b1b20db905948b3ec5dc13e1534e36.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e9f7e3acd21107fabeaca50f566a47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b1dfce6e66b7572073",
+    "url": "/static/css/main.ff4ffff1.chunk.css"
+  },
+  {
+    "revision": "eda278d9d19ed8a32ee0",
+    "url": "/static/js/2.2d21153d.chunk.js"
+  },
+  {
+    "revision": "b7b1dfce6e66b7572073",
+    "url": "/static/js/main.4f1da6d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e69fce8705b943d3f428427a4bc10140",
+    "url": "/static/media/argon-react-white.e69fce87.png"
+  },
+  {
+    "revision": "27b1b06134e7fedadb3964b131a72868",
+    "url": "/static/media/empty.27b1b061.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d216e9a60654c3776d4fb8150194b59d",
+    "url": "/static/media/header.d216e9a6.svg"
+  }
+]);
